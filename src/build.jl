53d3b042ba4c2d1d40141476694e47e595f973e3
Benoit Pasquier <pasquieb@uci.edu>
2019-12-12 10:58:15 +1100

    Add things to save

--- src/build.jl
+++ src/build.jl
@@ -14,11 +14,21 @@ include("problem_setup.jl")
 
 include("optimization.jl")
 
-# Solve for steady state and save solution
+# save solution, parameters, and mismatch
+DIP, POP = unpack_tracers(s_optimized, grd)
 DIPmismatch = 100sqrt(2AIBECS.mismatch(DIP, μDIPobs, σ²DIPobs, v))
-
 BSON.@save joinpath(@__DIR__, "..", "data", "optimized_solution.bson") s_optimized p_optimized DIPmismatch
 
+# save uptake
+U_PO₄ = U(DIP,p_optimized)
+BSON.@save joinpath(@__DIR__, "..", "data", "uptake.bson") U_PO₄ DIP POP
+
+# save parameters as variables
+function save_params(p)
+    @unpack w₀, w′, z₀, τPOP = p
+    BSON.@save joinpath(@__DIR__, "..", "data", "parameters.bson") z₀ w₀ w′ τPOP
+end
+save_params(p_optimized)
 #=
 To load the optimized model,
 
@@ -26,6 +36,6 @@ using AIBECS, BSON
 
 include("src/problem_setup.jl")
 
-BSON.@load "data/optimized_solution.bson" s_optimized p_optimized
+BSON.@load "data/optimized_solution.bson" s_optimized p_optimized DIPmismatch
 
 =#
